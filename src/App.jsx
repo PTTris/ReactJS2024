bdafcbb3900b329b45408d7f680c47e233baf53a
Phan Thanh Tri <tript2609@gmail.com>
2024-06-26 17:29:58 +0700

    Phan 6 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import { Outlet } from "react-router-dom";
 import "./App.css";
-import Todo from "./components/Todo/Todo";
+import Footer from "./components/Layout/Footer";
+import Header from "./components/Layout/Header";
 
 const App = () => {
     return (
         <>
-            <Todo />
+            <Header />
+            <Outlet />
+            <Footer />
         </>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import UsersPage from "./pages/Users.jsx";
+import Todo from "./components/Todo/Todo.jsx";
+import ErrorPage from "./components/Layout/ErrorPage.jsx";
+import BooksPage from "./pages/Books.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                index: true,
+                element: <Todo />,
+            },
+            {
+                path: "/users",
+                element: <UsersPage />,
+            },
+            {
+                path: "/books",
+                element: <BooksPage />,
+            },
+        ],
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
